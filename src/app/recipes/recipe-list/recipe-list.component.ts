dcb18672dd99c7fa72aec5328c4bd65bd7f41aef
hieptrinh96 <xmikecolx@gmail.com>
2023-02-22 00:51:21 -0600

    add more components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  constructor() { }
+recipes: Recipe[] = [
+  new Recipe('A test, Recipe', 'This is a simple test', 'https://www.justonecookbook.com/wp-content/uploads/2020/03/Okonomiyaki-Recipe-5681-II.jpg'),
+  new Recipe('A test, Recipe', 'This is a simple test', 'https://www.justonecookbook.com/wp-content/uploads/2020/03/Okonomiyaki-Recipe-5681-II.jpg')
+];
+  constructor() {}
 
   ngOnInit(): void {
-  }
+  } 
 
 }
